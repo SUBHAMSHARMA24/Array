6993581b2d054ee7a31a3cf6966285b08be98fb2
SUBHAMSHARMA24 <102910907+SUBHAMSHARMA24@users.noreply.github.com>
2023-08-05 11:29:07 +0530

    Create Greateroccurance.java

---
+++ Greateroccurance.java
@@ -0,0 +1,18 @@
+public class Greateroccurance
+{
+    static int Greaterocc(int arr[], int x){
+        int count=0;
+        int a=arr.length;
+        for(int i=0;i<a;i++){
+            if(arr[i]>x){
+                count+=1;;
+            }
+        }
+        return count;
+    }
+	public static void main(String[] args) {
+	    int arr[]={1,5,11,12,6};
+	    int x=5;
+		System.out.println("Strictly Greater occurrence: "+Greaterocc(arr,x));
+	}
+}
