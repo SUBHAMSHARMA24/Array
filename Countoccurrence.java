7f3c07d2e45fbb8264a5a70cac385491fc14fed6
SUBHAMSHARMA24 <102910907+SUBHAMSHARMA24@users.noreply.github.com>
2023-08-05 11:11:16 +0530

    Create Countoccurrence.java

---
+++ Countoccurrence.java
@@ -0,0 +1,23 @@
+public class Countoccurrence
+
+{
+    static int count(int[] arr, int x){
+        int count=0;
+     
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]==x){
+                count +=1;
+            
+            }
+            else{
+                count=count;
+            }
+        }
+        return count;
+    }
+	public static void main(String[] args) {
+	    int arr[]={5};
+	    int x=5;
+		System.out.println("No of occurrence"+count(arr,x));
+	}
+}
