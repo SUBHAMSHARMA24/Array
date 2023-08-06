874affdc8b491545bd2b8723c143e38056fe26ea
SUBHAMSHARMA24 <102910907+SUBHAMSHARMA24@users.noreply.github.com>
2023-08-06 12:55:18 +0530

    SumPair.java

--- SumPair.java
+++ SumPair.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 public class SumPair
 {
     static int pair(int arr[], int x){
-        Arrays.sort(arr);
         int count=0;
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
